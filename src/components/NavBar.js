445bfc254d384204ec6903aa15de558c2e93af2f
Facundo Perez <faacuu.peereezz@gmail.com>
2022-08-30 23:33:07 -0300

    Cambios para la segunda entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+const navBar = () => {
+    return (
+    <div>
+    <div class="header">
+        <div class="logo">
+            <img src="../img/Mountain.png" alt="Logo de la marca"></img>
+        </div>
+        <nav>
+           <ul class="nav-links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Sobre Nosotros</a></li>
+           </ul>            
+        </nav>
+        <a class="btn" href="#"><button>Iniciar Sesion</button></a>
+    
+    </div>
+    </div>
+    
+            ) 
+};
+export default navBar
