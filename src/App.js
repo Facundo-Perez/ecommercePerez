b2168c89e96d4bff2eddedc3e72d058082c64886
Facundo-Perez <faacuu.peereezz@gmail.com>
2022-09-13 23:34:38 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import NavBar from "./components/NavBar"
-import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 function App() {
   
